ca0d42dad40f49831abd465309d0c00c9645aa7a
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-08-16 18:34:35 -0700

    Completed all tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(sNames,eName){
+    let wroteCards = []
+    for(let x = 0;x<sNames.length;x++){
+        wroteCards.push(`Thank you, ${sNames[x]}, for the wonderful ${eName} gift!`)
+    }
+    return wroteCards
+}
+function countDown(end){
+    while(end >-1){
+        console.log(end)
+        end--
+    }
+}
